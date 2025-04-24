924d696dc7b725ee4811af16a70f7198558ba61b
AstridKMina <astridkmina@gmail.com>
2025-04-24 21:03:39 +0100

    Fixing img in topic table

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -1,5 +1,5 @@
 module.exports = [
-  { description: "Code is love, code is life", slug: "coding", img_url: "" },
+  { description: "Code is love, code is life", slug: "coding", img_url: "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" },
   {
     description: "FOOTIE!",
     slug: "football",
